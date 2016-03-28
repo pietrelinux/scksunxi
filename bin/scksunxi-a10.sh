1642030d37d6d98624d0e2cdb360659bdc83ffa9
pietrelinux <pietre.linux@gmail.com>
2016-03-28 10:56:26 +0200

    Update scksunxi-a10.sh

--- bin/scksunxi-a10.sh
+++ bin/scksunxi-a10.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 sudo apt-get install -y build-essential bin86 kernel-package libqt4-dev wget libncurses5 libncurses5-dev qt4-dev-tools libqt4-dev zlib1g-dev gcc-arm-linux-gnueabihf git debootstrap u-boot-tools device-tree-compiler 
 git clone -b sunxi-3.4 https://github.com/linux-sunxi/linux-sunxi.git
+tar -czvf linux-sunxi.tar.gz linux-sunxi/
 cd linux-sunxi
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- sun4i_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- xconfig
 ##For A10 use:
 #make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- sun4i_defconfig
 ##For A13 use:
