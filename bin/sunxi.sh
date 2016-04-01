699b220aa493b68813acabbd14968ac1a647b699
pietrelinux <pietre.linux@gmail.com>
2016-04-01 14:40:11 +0200

    Update sunxi.sh

--- bin/sunxi.sh
+++ bin/sunxi.sh
@@ -3,12 +3,12 @@
 sudo apt-get install -y build-essential bin86 kernel-package libqt4-dev wget libncurses5 libncurses5-dev qt4-dev-tools libqt4-dev zlib1g-dev gcc-arm-linux-gnueabihf git debootstrap u-boot-tools device-tree-compiler libusb-1.0-0-dev android-tools-adb android-tools-fastboot
 #Descarga kernel sunxi,el u-boot y lsd herramientas de sunxi
 git clone -b sunxi-3.4 https://github.com/linux-sunxi/linux-sunxi.git
-git clone -b sunxi https://github.com/linux-sunxi/u-boot-sunxi.git
+git clone git://git.denx.de/u-boot-sunxi.git
 git clone https://github.com/linux-sunxi/sunxi-tools
 #Comprime kernel 
 tar -czvf linux-sunxi.tar.gz linux-sunxi/
 # Comprime u-boot
-tar -czvf uboot.tar.gz u-boot-sunxi/
+tar -czvf u-boot-sunxi.tar.gz  u-boot-sunxi/
 #Comprime sunxi-tools
 tar -czvf sunxi-tools.tar.gz sunxi-tools/
 
@@ -35,9 +35,15 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=output modules
 +
 
 > uboot.sh
-cat <<+ > tools.sh
-cd u-boot-sunxi
-grep sunxi boards.cfg | awk '{print $7}'
+cat <<+ > uboot.sh
+#!/bin/sh
+cd u-boot-sunxi/
+echo "Now select your board configs to the folder and make CROSS_COMPILE=arm-linux-gnueabihf- <board_name>_defconfig"
+sleep 5
+cd u-boot-sunxi/
+ls configs
+echo "Remember,make CROSS_COMPILE=arm-linux-gnueabihf- <board_name>_defconfig"
+
 +
 
 > tools.sh
